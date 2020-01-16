0b7e67a75932fdf611f525dd45993e93dd273f3a
Yousef Rayyan <y.real.02@gmail.com>
2020-01-16 14:08:34 +0200

    Update

--- src/app/dyas-weather/days-weather.component.ts
+++ src/app/dyas-weather/days-weather.component.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-days-weather',
-  templateUrl: './days-weather.component.html',
-  styleUrls: ['./days-weather.component.css']
+    selector: 'app-days-weather',
+    templateUrl: './days-weather.component.html',
+    styleUrls: ['./days-weather.component.css']
 })
 export class DaysWeatherComponent implements OnInit {
 
-  @Input() forecasts;
-  public colors;
+    @Input() forecasts;
+    public colors;
 
-  constructor() {
-    this.colors = {
-      '01': '#5F4EEA',
-      '02': '#28E0AE',
-      '03': '#FF0090',
-      '04': '#FFAE01',
-      '09': '#2299F9',
-      '10': '#DA2727',
-      '11': '#D0DDF7',
-      '13': '#5F4EEA',
-      '50': '#5F4EEA'
-    };
-  }
+    constructor() {
+        this.colors = {
+            '01': '#5F4EEA',
+            '02': '#28E0AE',
+            '03': '#FF0090',
+            '04': '#FFAE01',
+            '09': '#2299F9',
+            '10': '#DA2727',
+            '11': '#D0DDF7',
+            '13': '#5F4EEA',
+            '50': '#5F4EEA'
+        };
+    }
 
-  ngOnInit() { }
+    ngOnInit() { }
 
 }
--- src/app/todays-weather/detailed-weather/detailed-weather.component.ts
+++ src/app/todays-weather/detailed-weather/detailed-weather.component.ts
@@ -2,30 +2,49 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-detailed-weather',
-  templateUrl: './detailed-weather.component.html',
-  styleUrls: ['./detailed-weather.component.css', '../todays-weather.component.css']
+    selector: 'app-detailed-weather',
+    templateUrl: './detailed-weather.component.html',
+    styleUrls: ['./detailed-weather.component.css', '../todays-weather.component.css']
 })
 export class DetailedWeatherComponent implements OnInit {
 
-  public todaysWeather;
-  public details;
+    public todaysWeather;
+    public details;
+    public objectKeys = Object.keys;
 
-  constructor(private route: ActivatedRoute) {
-  }
+    constructor(private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.todaysWeather = JSON.parse(params.todaysWeather);
-      // this.details = this.todaysWeather.main;
-      this.details = {
-        ...this.todaysWeather.main,
-        windSpeed: this.todaysWeather.wind.speed,
-        sunrise: this.todaysWeather.sys.sunrise,
-        sunset: this.todaysWeather.sys.sunset
-      };
-      console.log(this.details);
-    })
-  }
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.todaysWeather = JSON.parse(params.todaysWeather);
+            const sunsetDate = new Date(this.todaysWeather.sys.sunset);
+            const sunriseDate = new Date(this.todaysWeather.sys.sunrise);
+            this.details = [
+                { title: 'Sunset', value: this.getTime(sunsetDate) },
+                { title: 'Sunrise', value: this.getTime(sunriseDate) },
+                { title: 'Feels Like', value: this.todaysWeather.main.feels_like },
+                { title: 'Maximum Tempreture', value: this.todaysWeather.main.temp_max },
+                { title: 'Minimun Tempreture', value: this.todaysWeather.main.temp_min },
+                { title: 'Pressure', value: this.todaysWeather.main.pressure },
+                { title: 'Humidity', value: this.todaysWeather.main.humidity },
+                { title: 'Wind Speed', value: this.todaysWeather.wind.speed }
+            ];
+            console.log(this.details);
+        })
+    }
+
+    getTime(date: Date) {
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        let status;
+        if (hours >= 12) {
+            status = 'pm';
+        } else {
+            status = 'am';
+        }
+        return ('0' + hours).substr(('' + hours).length - 2, ('' + hours).length) + ':'
+             + ('0' + minutes).substr(('' + minutes).length - 2, ('' + minutes).length) + ' ' + status;
+    }
 
 }
